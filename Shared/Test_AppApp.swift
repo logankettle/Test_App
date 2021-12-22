0c8e1a3d2aea34137cdf2fe73f323e9ac3e05698
Logan Kettle <lgk24@me.com>
2021-12-22 11:56:47 -0600

    Initial Commit

---
+++ Shared/Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_AppApp.swift
+//  Shared
+//
+//  Created by Logan Kettle on 12/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
